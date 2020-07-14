9dca6919817906d8d38f7943c798e35b66e04302
victorvallet <victorvallet8@gmail.com>
2020-07-14 15:17:46 +0200

    Re added refactored test for #deposit and #withdraw

--- jasmine/spec/AccountSpec.js
+++ jasmine/spec/AccountSpec.js
@@ -13,5 +13,29 @@ describe('Bank account', () => {
     });
     it('has an initial empty statement', () => {
         expect(account.statement.length).toEqual(0)
-    })
+    });
+    it('can store multiple transactions', () => {
+        account.deposit(1000)
+        account.withdraw(200)
+        expect(account.statement).toEqual([{ debit: 0, credit: 1000, balance: 1000 }, { debit: 200, credit: 0, balance: 800 }])
+    });
+
+
+    describe('#deposit', () => {
+        it('increases the balance by an amount', () => {
+            account.deposit(300)
+            expect(account.balance).toBeGreaterThan(0)
+            expect(account.balance).toEqual(300)
+        });
+    });
+
+    describe('#withdraw', () => {
+        it('decreases the balance by an amount', () => {
+            account.withdraw(100)
+            expect(account.balance).toBeLessThan(0)
+            expect(account.balance).toEqual(-100)
+        });
+    });
+
+
 });
