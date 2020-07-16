4438b1f0ad4c7bb294be0203185a83c609769ff1
victorvallet <victorvallet8@gmail.com>
2020-07-16 17:29:31 +0200

    Refactored #addDebit #addCredit and date property with  new method

--- jasmine/src/Transaction.js
+++ jasmine/src/Transaction.js
@@ -1,16 +1,19 @@
 class Transaction {
     constructor() {
 
-        this.history = { debit: 0, credit: 0, balance: 0, date: new Date().toJSON().slice(0, 10).replace(/-/g, '/') }
+        this.history = { date: this.formatDate(), credit: 0, debit: 0, balance: 0 }
 
     }
 
     addDebit(money, balance) {
-        this.history.debit += money
+        this.history.debit = money
         this.history.balance = balance
     }
     addCredit(money, balance) {
-        this.history.credit += money
+        this.history.credit = money
         this.history.balance = balance
+    }
+    formatDate() {
+        return new Date().toLocaleDateString()
     }
 }
